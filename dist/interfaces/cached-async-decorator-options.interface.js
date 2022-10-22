767e5b2a3e22823149a4d968a39b2d149fe6d365
Stimul Cross <stimulcross@gmail.com>
2022-10-23 00:34:18 +0600

    chore: delete dist directory

--- dist/interfaces/cached-async-decorator-options.interface.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=cached-async-decorator-options.interface.js.map
--- dist/utils/is-object.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.isObject = void 0;
-function isObject(val) {
-    return Object.prototype.toString.call(val) === '[object Object]';
-}
-exports.isObject = isObject;
-//# sourceMappingURL=is-object.js.map
