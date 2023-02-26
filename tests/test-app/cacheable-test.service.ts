83c868f3138711d1201a7a46918f6691291470e2
StimulCross <stimulcross@gmail.com>
2023-02-26 15:44:50 +0600

    chore(lint): update eslint rules to allow underscored unused params

--- tests/test-app/cacheable-test.service.ts
+++ tests/test-app/cacheable-test.service.ts
@@ -10,26 +10,22 @@ export class CacheableTestService {
 	[CACHE_INSTANCE_ID_PROPERTY]!: number;
 
 	@Cached({ ttl: 100 })
-	// eslint-disable-next-line @typescript-eslint/no-unused-vars
-	public getRandomNumber(options?: CacheArgumentOptions): number {
+	public getRandomNumber(_options?: CacheArgumentOptions): number {
 		return Math.random();
 	}
 
 	@Cached({ ttl: 100, useSharedCache: true })
-	// eslint-disable-next-line @typescript-eslint/no-unused-vars
-	public getRandomNumberShared(options?: CacheArgumentOptions): number {
+	public getRandomNumberShared(_options?: CacheArgumentOptions): number {
 		return Math.random();
 	}
 
 	@Cached({ ttl: 100, useArgumentOptions: true })
-	// eslint-disable-next-line @typescript-eslint/no-unused-vars
-	public getRandomNumberWithOptions(options?: CacheArgumentOptions): number {
+	public getRandomNumberWithOptions(_options?: CacheArgumentOptions): number {
 		return Math.random();
 	}
 
 	@CachedAsync({ ttl: 100 })
-	// eslint-disable-next-line @typescript-eslint/no-unused-vars
-	public async getRandomNumberAsync(options?: CacheArgumentOptions): Promise<number> {
+	public async getRandomNumberAsync(_options?: CacheArgumentOptions): Promise<number> {
 		return Math.random();
 	}
 
@@ -39,8 +35,7 @@ export class CacheableTestService {
 	}
 
 	@CachedAsync({ ttl: 100, useArgumentOptions: true })
-	// eslint-disable-next-line @typescript-eslint/no-unused-vars
-	public async getRandomNumberAsyncWithOptions(options?: CacheArgumentOptions): Promise<number> {
+	public async getRandomNumberAsyncWithOptions(_options?: CacheArgumentOptions): Promise<number> {
 		return Math.random();
 	}
 }
